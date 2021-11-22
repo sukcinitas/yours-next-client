fe9d17440bad8cac4d0c23ba15f989e11baed507
Gintare Cerniauskaite <gintare.cerniauskaite@protonmail.com>
2021-11-22 20:28:57 +0200

    Use http protocol over https

--- src/components/RedirectionComp.vue
+++ src/components/RedirectionComp.vue
@@ -1,6 +1,6 @@
 <template>
     <h1 class="redirect">Please visit here
-      <a href="http://yours-next.herokuapp.com" class="redirect__link" target="_blank" rel="noreferrer noopener">http://yours-next.herokuapp.com</a>
+      <a href="https://yours-next.herokuapp.com" class="redirect__link" target="_blank" rel="noreferrer noopener">https://yours-next.herokuapp.com</a>
     </h1>
 </template>
 
